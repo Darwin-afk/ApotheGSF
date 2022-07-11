efab23d105fa33d8007e2b38d027fbb096d392a1
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-07-11 16:03:04 -0400

    Agregando campos a AppRol

--- ApotheGSF/Models/AppRol.cs
+++ ApotheGSF/Models/AppRol.cs
@@ -4,5 +4,11 @@ namespace ApotheGSF.Models
 {
     public class AppRol : IdentityRole<int>
     {
+        public int GetRolId()
+        {
+            return this.GetRolId();
+        }
+
+        public virtual ICollection<AppUsuarioRol> UsuariosRoles { get; set; }
     }
 }

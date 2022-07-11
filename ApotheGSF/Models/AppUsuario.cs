3920b45edd99417a0af3970f84f12ecc98914461
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-07-11 16:12:36 -0400

    Agregando campos a AppUsuario

--- ApotheGSF/Models/AppUsuario.cs
+++ ApotheGSF/Models/AppUsuario.cs
@@ -6,6 +6,25 @@ namespace ApotheGSF.Models
 {
     public class AppUsuario : IdentityUser<int>
     {
-        public ICollection<AppUsuarioRol> UserRoles { get; set; }
+        [Required]
+        [Column("Nombre", TypeName = "varchar(50)")]
+        public string Nombre { get; set; }
+        [Required]
+        [Column("Apellido", TypeName = "varchar(50)")]
+        public string Apellido { get; set; }
+        [Column("Foto", TypeName = "varchar(50)")]
+        public string? Foto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        [NotMapped]
+        [Display(Name = "Rol")]
+        public string? Rol { get; set; }
+        public ICollection<AppUsuarioRol> UsuariosRoles { get; set; }
+        public DateTime Creado { get; set; }
+        public int CreadoPorId { get; set; }
+        public DateTime? Modificado { get; set; }
+        public int? ModificadoPorId { get; set; }
+        public bool Inactivo { get; set; }
     }
 }

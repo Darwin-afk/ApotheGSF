232a5ae8a04ac6bffe6a33c28165063977de2627
ErickVO <erick_vicente@ucne.edu.do>
2022-07-12 00:58:38 -0400

    Arreglando Errores

--- ApotheGSF/Models/MedicamentosCajas.cs
+++ ApotheGSF/Models/MedicamentosCajas.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApotheGSF.Models
 {
     public class MedicamentosCajas
     {
+        [Key]
         public int CajaId { get; set; }
         [ForeignKey("Medicamentos")]
-        public virtual Medicamentos? CodigoMedicamento { get; set; }
+        public virtual Medicamentos? MedicamentosId { get; set; }
         [NotMapped]
         public string? NombreMedicamentos { get; set; }
         public DateTime FechaAdquirido { get; set; }
--- ApotheGSF/Models/ProveedorMedicamentos.cs
+++ ApotheGSF/Models/ProveedorMedicamentos.cs
@@ -9,15 +9,14 @@ namespace ApotheGSF.Models
         public int ProveedorMedicamentoId { get; set; }
 
         [ForeignKey("Proveedores")]
-        public int ProveedorId { get; set; }
-        public virtual Proveedores? Proveedores { get; set; }
+        public virtual Proveedores? ProveedoresId { get; set; }
         [NotMapped]
         public string? NombreProveedores { get; set; }
         [ForeignKey("Medicamentos")]
-        public int MedicamentoId { get; set; }
+        public virtual Medicamentos? MedicamentosId { get; set; }
         [NotMapped]
         public string? NombreMedicamentos { get; set; }
-        public virtual Medicamentos? Medicamentos { get; set; }
+        
         
         public int CantidadCaja { get; set; }
         public int CantidadUnidad { get; set; }

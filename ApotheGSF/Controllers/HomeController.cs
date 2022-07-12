3f24335b8300cdb10725ea436d5211b3c094ef92
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-07-11 21:43:36 -0400

    Agregando el PerfilUsuario a HomeController.cs

--- ApotheGSF/Controllers/HomeController.cs
+++ ApotheGSF/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using ApotheGSF.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Diagnostics;
 using System.Security.Claims;
 
@@ -15,16 +16,22 @@ namespace ApotheGSF.Controllers
         private readonly SignInManager<AppUsuario> _signInManager;
         private readonly UserManager<AppUsuario> _userManager;
         private readonly ClaimsPrincipal _user;
+        private readonly string webRoot;
+        private readonly IOptions<AppSettings> appSettings;
 
         public HomeController(ILogger<HomeController> logger,
                               SignInManager<AppUsuario> signInManager,
                               UserManager<AppUsuario> userManager,
-                              IHttpContextAccessor accessor)
+                              IHttpContextAccessor accessor,
+                              IWebHostEnvironment env,
+                              IOptions<AppSettings> _appSettings)
         {
             _logger = logger;
             _signInManager = signInManager;
             _userManager = userManager;
             _user = accessor.HttpContext.User;
+            webRoot = env.WebRootPath;
+            appSettings = _appSettings;
         }
 
         public IActionResult Index()
@@ -43,6 +50,7 @@ namespace ApotheGSF.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        #region LogIn
         [HttpGet]
         public async Task<IActionResult> Login(string returnUrl = "")
         {
@@ -95,6 +103,8 @@ namespace ApotheGSF.Controllers
             ModelState.AddModelError("", "Usuario/Contraseña Inválidos");
             return View(model);
         }
+
+        #endregion
 
         [Authorize]
         public async Task<IActionResult> Logout()
@@ -155,7 +165,126 @@ namespace ApotheGSF.Controllers
             return View(modelo);
         }
 
+        #endregion
 
+        #region PerfilUsuario
+        public async Task<IActionResult> PerfilUsuario(int? id)
+        {
+            if (id == null)
+            {
+                Response.StatusCode = 404;
+                return View("NotFound");
+            }
+
+            AppUsuario modelo = await _userManager.FindByIdAsync(id.ToString());
+
+            if (modelo == null)
+            {
+                Response.StatusCode = 404;
+                return View("NotFound");
+                //return NotFound();
+            }
+            PerfilUsuarioViewModel perfil = new PerfilUsuarioViewModel()
+            {
+                Nombre = modelo.Nombre,
+                Email = modelo.Email,
+                Telefono = modelo.PhoneNumber,
+                Foto = modelo.Foto
+            };
+
+            return View(perfil);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PerfilUsuario([Bind("Id, Nombre, Email, Telefono, Foto")] PerfilUsuarioViewModel modelo, IFormFile logo, string removeLogo)
+        {
+            //Valida que si se cambia el correo no exista otro usuario con el mismo asignado.
+            var u = await _userManager.FindByEmailAsync(modelo.Email); //No se puede registrar el mismo correo en el sistema dos veces, no importa la Org.
+            if (u != null && u.Id != modelo.Id)
+            {
+                ModelState.AddModelError("", string.Format("El correo {0} ya está registrado.", modelo.Email));
+            }
+
+            if (!modelo.Email.IsValidEmail())
+            {
+                ModelState.AddModelError("", string.Format("El Email {0} no es correcto.", modelo.Email));
+            }
+
+            ModelState.Remove("Foto");
+            //if (ModelState.IsValid)
+            //{
+            //    var user = await _userManager.FindByIdAsync(modelo.Id.ToString());
+            //    if (user != null)
+            //    {
+
+
+            //        if (removeLogo.Equals("1") || logo != null)
+            //        {
+            //            if (user.Foto != null)
+            //            {
+            //                string archivoActual = Path.Combine(webRoot, appSettings.Value.RutaImagenesUsers, user.Foto);
+            //                if (System.IO.File.Exists(archivoActual))
+            //                {
+            //                    try
+            //                    {
+            //                        System.GC.Collect(); ///Si no se hace esto dice algunas veces quel archivo esta siendo utilizado por otro proceso
+            //                        System.GC.WaitForPendingFinalizers();
+            //                        System.IO.File.Delete(archivoActual);
+
+            //                    }
+            //                    catch (Exception e)
+            //                    {
+            //                        _logger.LogDebug("PerfilUsuario", e);
+            //                    }
+            //                }
+            //            }
+            //            user.Foto = null;
+            //        }
+            //        if (logo != null && removeLogo.Equals("0"))
+            //        {
+            //            string fileExtension = Path.GetExtension(logo.FileName);
+            //            string fileName = Guid.NewGuid() + fileExtension;
+            //            user.Foto = fileName;
+            //            fileName = Path.Combine(webRoot, appSettings.Value.RutaImagenesUsers, fileName);
+            //            logo.CopyTo(new FileStream(fileName, FileMode.Create));
+            //        }
+            //        user.Nombre = modelo.Nombre;
+            //        user.Email = modelo.Email;
+            //        user.PhoneNumber = modelo.Telefono;
+            //        user.Modificado = DateTime.Now;
+            //        user.ModificadoPorId = User.GetUserID().ToInt();
+            //        modelo.Foto = user.Foto;
+            //    }
+            //    var result = await _userManager.UpdateAsync(user); //actualiza el usuario
+            //    if (!result.Succeeded)
+            //    {
+            //        foreach (var error in result.Errors)
+            //        {
+            //            ModelState.AddModelError(string.Empty, error.Description);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        ///Si todo esta bien colocamos la foto en el claim
+            //        var userClaims = await _userManager.GetClaimsAsync(user); //busca todos los claims del usuario modificado
+
+            //        ///elimina los claims nombre y posicion, el idOrganizacion no cambia
+            //        await _userManager.RemoveClaimsAsync(user, userClaims.Where(x => x.Type.Equals("Foto")));
+            //        await _userManager.RemoveClaimsAsync(user, userClaims.Where(x => x.Type.Equals("Nombre")));
+
+            //        ///agrega los claims nuevamente
+            //        await _userManager.AddClaimAsync(user, new Claim("Nombre", user.Nombre));
+            //        if (user.Foto != null)
+            //            await _userManager.AddClaimAsync(user, new Claim("Foto", user.Foto));
+
+            //        await _signInManager.RefreshSignInAsync(user);
+            //        return RedirectToAction("PerfilUsuario", "Home");
+            //    }
+
+            //}
+            return View(modelo);
+        }
         #endregion
     }
 }

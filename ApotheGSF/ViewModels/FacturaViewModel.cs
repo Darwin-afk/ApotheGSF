5ee5dd9287ae7b254571d58f639a65854847110c
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-07-16 15:11:08 -0400

    Agregando FacturaViewModel.cs a ViewModels

---
+++ ApotheGSF/ViewModels/FacturaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ApotheGSF.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApotheGSF.ViewModels
+{
+    public class FacturaViewModel
+    {
+        public int Codigo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public float SubTotal { get; set; }
+        public float Total { get; set; }
+        public string? Estado { get; set; }
+        public List<FacturaMedicamentos> Medicamentos { get; set; }
+        public DateTime? Creado { get; set; }
+        public int? CreadoId { get; set; }
+        public DateTime? Modificado { get; set; }
+        public int? ModificadoId { get; set; }
+    }
+}

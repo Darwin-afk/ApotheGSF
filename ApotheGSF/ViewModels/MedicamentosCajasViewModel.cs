22c8b37c1a4e20a0b61b3d21cc434fb594cc3d36
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-09 16:20:13 -0400

    agregando validaciones a medicamentosCajas

--- ApotheGSF/ViewModels/MedicamentosCajasViewModel.cs
+++ ApotheGSF/ViewModels/MedicamentosCajasViewModel.cs
@@ -1,15 +1,21 @@
-﻿namespace ApotheGSF.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApotheGSF.ViewModels
 {
     public class MedicamentosCajasViewModel
     {
         public int CodigoCaja { get; set; }
         public int CodigoMedicamento { get; set; }
         public int CantidadUnidad { get; set; }
+        [Required(ErrorMessage = "Digite la fecha de adquisición")]
         public DateTime FechaAdquirido { get; set; }
+        [Required(ErrorMessage = "Digite la fecha de vencimiento")]
         public DateTime FechaVencimiento { get; set; }
         public bool Detallada { get; set; }
         public bool Inactivo { get; set; }
         public string? NombreMedicamento { get; set; }
+        [Required(ErrorMessage = "Digite una cantidad")]
         public int Cajas { get; set; }
     }
 }

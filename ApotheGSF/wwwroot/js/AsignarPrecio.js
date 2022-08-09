e278c7d262b37a866c99564d6fb9038659002a97
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-08 21:23:31 -0400

    modificando la asignacion de precio en medicamentos

--- ApotheGSF/wwwroot/js/AsignarPrecio.js
+++ ApotheGSF/wwwroot/js/AsignarPrecio.js
@@ -1,4 +1,12 @@
 ﻿function AsignarPrecio() {
-    document.getElementById('PrecioUnidad').value = (document.getElementById('Costo').value * 1.10).toFixed(0);
+    var costo = document.getElementById('Costo').value;
+    var unidades = document.getElementById('UnidadesCaja').value;
+
+    if (costo > 0 && unidades > 0) {
+        document.getElementById('PrecioUnidad').value = ((costo * 1.10) / unidades).toFixed(2);
+    }
+    else {
+        document.getElementById('PrecioUnidad').value = 0
+    }
 
 };

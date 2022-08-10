9b1f227950b842fc2bd12e9812fa5cb42dfdd641
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-09 21:50:08 -0400

    Agregando mensaje de guardado exitosamente a facturar

--- ApotheGSF/Clases/ResultadoFactura.cs
+++ ApotheGSF/Clases/ResultadoFactura.cs
@@ -4,5 +4,6 @@
     {
         public int codigofactura { get; set; }
         public bool resultado { get; set; }
+        public string mensaje { get; set; }
     }
 }
--- ApotheGSF/wwwroot/js/Facturar.js
+++ ApotheGSF/wwwroot/js/Facturar.js
@@ -7,7 +7,7 @@
             if (r.resultado) {
                 var url = 'ReporteFactura?CodigoFactura=' + r.codigofactura;
                 window.open(url, "_blank");
-                window.location.href = "/Home/Index";
+                window.location.href = "/Home/Index?Mensaje=" + r.mensaje;
             }
         }
     });

5a2a91565342e39687ac2de9ad4e57468bf9a348
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-11 01:06:34 -0400

    modificando

--- ApotheGSF/wwwroot/js/AgregarFacturaDetalle.js
+++ ApotheGSF/wwwroot/js/AgregarFacturaDetalle.js
@@ -9,6 +9,7 @@
 
                 document.getElementById('SubTotal').value = resultado.subtotal.toFixed(2);
                 document.getElementById('Total').value = (resultado.subtotal * 1.18).toFixed(2);
+                document.getElementById('itbis').value = ((resultado.subtotal * 1.18)* 0.18).toFixed(2);
             }
         }
     });

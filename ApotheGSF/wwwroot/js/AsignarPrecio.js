56b1d2a51bcb1c66578a80b9d8c0038c68c29b9b
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-18 21:28:45 -0400

    modificando cajas controller

--- ApotheGSF/wwwroot/js/AsignarPrecio.js
+++ ApotheGSF/wwwroot/js/AsignarPrecio.js
@@ -1,12 +1,8 @@
 ﻿function AsignarPrecio() {
-    var costo = document.getElementById('Costo').value;
-    var unidades = document.getElementById('UnidadesCaja').value;
+    alert("prueba 2");
 
-    if (costo > 0 && unidades > 0) {
-        document.getElementById('PrecioUnidad').value = ((costo * 1.10) / unidades).toFixed(2);
-    }
-    else {
-        document.getElementById('PrecioUnidad').value = 0.00
-    }
+    document.getElementById('PrecioUnidad').value = 8.00
+
+
 
 };

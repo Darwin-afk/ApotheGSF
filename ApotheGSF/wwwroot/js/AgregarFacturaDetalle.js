02773de537ada47df2921e90b0c7969c4d567a9e
Darwin Lantigua Fermin <darwin_lantigua@ucne.edu.do>
2022-08-20 16:58:37 -0400

    limpiar datos de agregar luego de agregar

--- ApotheGSF/wwwroot/js/AgregarFacturaDetalle.js
+++ ApotheGSF/wwwroot/js/AgregarFacturaDetalle.js
@@ -10,6 +10,20 @@
                 document.getElementById('SubTotal').value = resultado.subtotal.toFixed(2);
                 document.getElementById('Total').value = (resultado.subtotal * 1.18).toFixed(2);
                 document.getElementById('itbis').value = (resultado.subtotal * 0.18).toFixed(2);
+
+                alert("prueba 1");
+
+                var selector = $('#MedicamentoId').selectize().data('selectize');
+                selector.setValue('', false);
+
+                var selector2 = $('#LaboratorioId').selectize().data('selectize');
+                selector2.setValue('', false);
+                selector2.clearOptions();
+
+                var selector3 = $('#TipoCantidad').selectize().data('selectize');
+                selector3.setValue('1', false);
+
+                document.getElementById('Cantidad').value = 0;
             }
         }
     });
